9818919a02f8223cd7f8fd27b8b862e18302f065
rishad <rishadnm@ix.lentra.ai>
2023-09-24 22:27:20 +0530

    added a score and a submit button

--- app.py
+++ app.py
@@ -18,20 +18,26 @@ def main():
 
     question_flags = [False] * len(quiz_data)
 
+    selected_options = [-1] * len(quiz_data)
+
     for i, question_data in enumerate(quiz_data, start=1):
         st.subheader(f"Question {i}: {question_data['question']}")
-        
-        selected_option = st.selectbox("Choose an option:", [""] + question_data['options'], key=f"q{i}")
-        
+
+        selectbox_key = f"q{i}_selectbox"
+
+        selected_option = st.selectbox("Choose an option:", [""] + question_data['options'], key=selectbox_key)
+
         if selected_option:
             selected_option_index = question_data['options'].index(selected_option)
-            if selected_option_index == question_data['correct_option']:
-                question_flags[i - 1] = True
-            else:
-                pass
+            selected_options[i - 1] = selected_option_index
 
-    # for i, flag in enumerate(question_flags, start=1):
-    #     st.write(f"Q{i}_flag: {flag}")
+    if st.button("Submit"):
+        for i, question_data in enumerate(quiz_data):
+            if selected_options[i] == question_data['correct_option']:
+                question_flags[i] = True
+
+        st.write(f"Your quiz score is:{question_flags.count(True)}")
+        
 
 if __name__ == "__main__":
     main()
